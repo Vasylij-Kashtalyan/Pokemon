aa81c19245e680322c953f79e8f08746192a2e5d
Vasylij-Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2022-12-28 18:34:57 +0200

    Updates

---
+++ static/js/769.0a3d082c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcard_pokemon=self.webpackChunkcard_pokemon||[]).push([[769],{8769:function(n,e,r){r.r(e),r.d(e,{default:function(){return t}});var o=r(184);var t=function(){return(0,o.jsx)("h1",{children:"Oops, something went wrong :("})}}}]);
+//# sourceMappingURL=769.0a3d082c.chunk.js.map
