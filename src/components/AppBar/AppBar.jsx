9005a2ee9b4fd404ed1e60f0123a8ce91a518f10
Vasylij-Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2023-01-12 23:00:57 +0200

    cler cont

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,7 +1,6 @@
 import s from './AppBar.module.scss';
 import { PokeBol } from 'components/AppBar/PokeBol/PokeBol';
 import { Logo } from './Logo/Logo';
-import { Container } from '../Container/Container';
 
 function AppBar() {
   return (
